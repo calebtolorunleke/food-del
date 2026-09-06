e0580f197cbca0671eb2ccc766b9c152c77ade18
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-09-06 06:50:08 -0700

    initial commit

--- routes/profileRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getProfile } = require("../controller/profileController");
-
-const router = express.Router();
-
-router.get("/him", getProfile);
-
-module.exports = router;
