d95ff2441f44f19e0b42a9e4de255746aa62a2b8
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-09-06 22:51:47 -0700

    feat: refactored the navbar ui to be responsive on mobile and other screens

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Navbar = () => {
-  return <div className="">Navbar</div>;
+  return (
+    <div className="">
+      <img src={assets.logo} alt="" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile-app</li>
+        <li>Contact Us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign In</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
