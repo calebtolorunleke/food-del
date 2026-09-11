f63735024f4be50cb8ceddd45d346377518e245e
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-09-11 16:40:36 -0700

    feat: implement the UI design for Food item and food display

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -3,13 +3,16 @@ import { assets } from "../../assets/assets";
 
 const FoodItem = ({ id, name, price, description, image }) => {
   return (
-    <div className="food-item" key={id}>
+    <div
+      key={id}
+      className="food-item w-full m-auto rounded-[15px] shadow-[0_0_10px_rgba(0,0,0,0.15)] transition-all duration-300 animate-fadeIn overflow-x-hidden"
+    >
       <div className="food-item-img-container">
-        <img src={image} alt={name} className="food-item-image" />
+        <img src={image} alt={name} className="food-item-image w-[100%]" />
       </div>
-      <div>
-        <div className="food-item-rating">
-          <p>{name}</p>
+      <div className="food-tem-info p-[20px]">
+        <div className="food-item-rating flex justify-between items-center mb-[10px]">
+          <p className="text-[20px] font-medium">{name}</p>
           <img src={assets.rating_starts} alt="" />
         </div>
         <p className="food-item-desc">{description}</p>
