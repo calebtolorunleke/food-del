3f284e4ed8ec5cce000b0879133ee97335234c3d
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-09-11 23:19:04 -0700

    feat: Implemet the full footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div className="footer text-[#d9d9d9] bg-[#323232] flex flex-col  gap-5  py-[20px] px-[8vw] pt-20 ">
+    <div className="footer text-[#d9d9d9] bg-[#323232] flex flex-col  gap-5  py-[20px] px-[8vw] pt-20 mt-[100px] ">
       <div className="footer-content w-full grid grid-cols-[2fr_1fr_1fr] gap-[80px]">
         <div className="footer-content-left flex flex-col items-start gap-[20px]">
           <img src={assets.logo} alt="" />
@@ -12,14 +12,26 @@ const Footer = () => {
             obcaecati consequatur quos eius harum officiis veniam fuga rem?
             Iste, architecto?
           </p>
-          <div className="footer-social-icons">
-            <img src={assets.facebook_icon} alt="" />
-            <img src={assets.twitter_icon} alt="" />
-            <img src={assets.linkedin_icon} alt="" />
+          <div className="footer-social-icons flex">
+            <img
+              src={assets.facebook_icon}
+              alt=""
+              className="w-[40px] mr-[15px]"
+            />
+            <img
+              src={assets.twitter_icon}
+              alt=""
+              className="w-[40px] mr-[15px]"
+            />
+            <img
+              src={assets.linkedin_icon}
+              alt=""
+              className="w-[40px] mr-[15px]"
+            />
           </div>
         </div>
         <div className="footer-content-center flex flex-col items-start gap-[20px]">
-          <h2 className="text-white">COMPANY</h2>
+          <h2 className="text-white font-semibold">COMPANY</h2>
           <ul>
             <li className="mb-[10px]">Home</li>
             <li className="mb-[10px]">About us</li>
@@ -29,14 +41,14 @@ const Footer = () => {
         </div>
 
         <div className="footer-content-right flex flex-col items-start gap-[20px]">
-          <h2 className="text-white">GET IN Touch</h2>
+          <h2 className="text-white font-semibold">GET IN Touch</h2>
           <ul>
             <li className="mb-[10px]">+1-212-456-7890</li>
             <li className="mb-[10px]">contact@tomato.com</li>
           </ul>
         </div>
       </div>
-      <hr />
+      <hr className="w-[100%] h-[2px] my-[20px] bg-gray-500" />
       <p className="footer-copyright">
         Copyright {new Date().getFullYear()} &copy; Tomato.com - All Right
         Reserved
