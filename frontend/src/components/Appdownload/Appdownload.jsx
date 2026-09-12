7aab9b36eaa16a93c246815f6e4e2abbf48b7fb8
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-09-11 23:34:18 -0700

    feat: refactor the app download component into the home page

--- frontend/src/components/Appdownload/Appdownload.jsx
+++ frontend/src/components/Appdownload/Appdownload.jsx
@@ -3,13 +3,25 @@ import { assets } from "../../assets/assets";
 
 const Appdownload = () => {
   return (
-    <div className="app-download m-auto mt-[100px] text-center flex justify-center">
-      <p>
-        For better Expereince Download <br /> Tomato App
+    <div
+      className="app-download m-auto mt-[100px] text-center flex flex-col items-center justify-center gap-6 font-medium text-[max(3vw,20px)]"
+      id="app-download"
+    >
+      <p className="leading-snug text-[#262626]">
+        For Better Experience Download <br /> Tomato App
       </p>
-      <div className="app-download-platforms">
-        <img src={assets.play_store} alt="" />
-        <img src={assets.app_store} alt="" />
+
+      <div className="app-download-platforms flex justify-center gap-[max(2vw,10px)] mt-2">
+        <img
+          src={assets.play_store}
+          alt="Play Store"
+          className="w-[max(12vw,120px)] max-w-[180px] cursor-pointer transition-transform duration-300 hover:scale-105"
+        />
+        <img
+          src={assets.app_store}
+          alt="App Store"
+          className="w-[max(12vw,120px)] max-w-[180px] cursor-pointer transition-transform duration-300 hover:scale-105"
+        />
       </div>
     </div>
   );
