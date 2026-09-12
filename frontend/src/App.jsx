c8628731e9016dd16144cc3ae3452bdd2c2e7a45
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-09-11 22:27:25 -0700

    feat: Implemet the full footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,22 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import Placeorder from "./pages/PlaceOrder/Placeorder";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className="mx-auto w-[80%]">
+    <>
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order" element={<Placeorder />} />
-      </Routes>
-    </div>
+      <div className="mx-auto w-[80%]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<Placeorder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 };
 
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer ">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
+            obcaecati consequatur quos eius harum officiis veniam fuga rem?
+            Iste, architecto?
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-right">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div className="footer-content-right">
+          <h2>GET IN Touch</h2>
+          <ul>
+            <li>+1-212-456-7890</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright {Date.toString()} %; Tomato.com - All Right Reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
