24ec2fb05d86d6fa1a10b7fd4f74ae6a89554c30
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-09-11 22:39:09 -0700

    feat: Implemet the full footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div className="footer ">
-      <div className="footer-content">
-        <div className="footer-content-left">
+    <div className="footer text-[#d9d9d9] bg-[#323232] flex flex-col  gap-5  py-[20px] px-[8vw] pt-20 ">
+      <div className="footer-content w-full grid grid-cols-[2fr_1fr_1fr] gap-[80px]">
+        <div className="footer-content-left flex flex-col items-start gap-[20px]">
           <img src={assets.logo} alt="" />
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
@@ -18,21 +18,21 @@ const Footer = () => {
             <img src={assets.linkedin_icon} alt="" />
           </div>
         </div>
-        <div className="footer-content-right">
-          <h2>COMPANY</h2>
+        <div className="footer-content-center flex flex-col items-start gap-[20px]">
+          <h2 className="text-white">COMPANY</h2>
           <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
-            <li>Privacy policy</li>
+            <li className="mb-[10px]">Home</li>
+            <li className="mb-[10px]">About us</li>
+            <li className="mb-[10px]">Delivery</li>
+            <li className="mb-[10px]">Privacy policy</li>
           </ul>
         </div>
 
-        <div className="footer-content-right">
-          <h2>GET IN Touch</h2>
+        <div className="footer-content-right flex flex-col items-start gap-[20px]">
+          <h2 className="text-white">GET IN Touch</h2>
           <ul>
-            <li>+1-212-456-7890</li>
-            <li>contact@tomato.com</li>
+            <li className="mb-[10px]">+1-212-456-7890</li>
+            <li className="mb-[10px]">contact@tomato.com</li>
           </ul>
         </div>
       </div>
