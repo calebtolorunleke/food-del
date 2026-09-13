7db594c6b587232df7dc4e10ae63fcc90035eadd
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-09-12 21:59:11 -0700

    feat: implement authmodal for login and signup

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { assets } from "../../assets/assets";
 
 const LoginPopup = ({ setShowLogin }) => {
-  const [currState, setCurrState] = useState("Sign Up");
+  const [currState, setCurrState] = useState("Login");
 
   return (
     <div className="login-popup">
@@ -16,11 +16,19 @@ const LoginPopup = ({ setShowLogin }) => {
           />
         </div>
         <div className="login-popup-inputs">
-          <input type="text" placeholder="Your name" required />
-          <input type="email" placeholder="Your name" required />
-          <input type="password" placeholder="Your name" required />
+          {currState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your name" required />
+          )}
+          <input type="email" placeholder="Your email" required />
+          <input type="password" placeholder="Password" required />
         </div>
         <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
       </form>
     </div>
   );
